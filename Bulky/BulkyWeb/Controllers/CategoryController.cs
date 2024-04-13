b63cb11490a7108fe09ca64505a461d6836de3aa
shajigholam <shajigholam@myseneca.ca>
2024-04-13 19:15:36 -0400

    Updated CRUD

--- Bulky/BulkyWeb/Controllers/CategoryController.cs
+++ Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -24,7 +24,7 @@ namespace BulkyWeb.Controllers
             return View(objCategoryList);
         }
 
-        // create an action method
+        // create a get action method
         public IActionResult Create()
         {
             // by default passing a new Category obj
@@ -35,7 +35,54 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
+            // custom validation
+            //if (obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    // key is name, so the custom error is shown for this field
+            //    ModelState.AddModelError("name", "The dispaly order cannot exactly match the name.");
+            //}
+            if (ModelState.IsValid)
+            {
+                // add a new Category obj
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
             return View();
+            
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id==id);
+            //Category? categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        // when hit submit button
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                // update the Category obj
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
         }
     }
 }
