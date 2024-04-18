caca321121b9d98401ba4f2a23dcbf8d961de934
shajigholam <shajigholam@myseneca.ca>
2024-04-18 15:36:33 -0400

    Added Delete & Update Razor Pages

---
+++ Bulky/BulkyWebRazor_Temp/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor_Temp.Pages.Categories
+{
+    [BindProperties]
+	public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public Category Category { get; set; }
+
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet(int? id)
+        {
+            if (id != null && id != 0)
+            {
+                Category = _db.Categories.Find(id);
+            }
+            
+        }
+        public IActionResult OnPost()
+        {
+            if (Category == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(Category);
+            _db.SaveChanges();
+            return RedirectToPage("Index");
+        }
+    }
+}
