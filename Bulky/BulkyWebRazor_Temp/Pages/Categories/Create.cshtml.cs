3e493c0c477db098e753de1f36d52435e3b6622e
shajigholam <shajigholam@myseneca.ca>
2024-04-18 13:36:10 -0400

    added create razor page

--- Bulky/BulkyWebRazor_Temp/Pages/Categories/Create.cshtml.cs
+++ Bulky/BulkyWebRazor_Temp/Pages/Categories/Create.cshtml.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BulkyWebRazor_Temp.Pages.Categories
 {
+    [BindProperties]
+
 	public class CreateModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+
+        public Category Category { get; set; }
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            // dependency injection
+            _db = db;
+        }
         public void OnGet()
         {
+        }
+        // post handler
+        public IActionResult OnPost()
+        {
+            _db.Categories.Add(Category);
+            _db.SaveChanges();
+            return RedirectToPage("Index");
         }
     }
 }
