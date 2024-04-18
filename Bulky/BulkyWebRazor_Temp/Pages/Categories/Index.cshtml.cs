53d508d10b22e5dda05352a68693b05dd699d8ad
shajigholam <shajigholam@myseneca.ca>
2024-04-18 10:35:41 -0400

    Initial Commit for Razor Proj

---
+++ Bulky/BulkyWebRazor_Temp/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor_Temp.Pages.Categories
+{
+	public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public List<Category> CategoryList { get; set; }
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            // dependency injection
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+            CategoryList = _db.Categories.ToList();
+        }
+    }
+}
