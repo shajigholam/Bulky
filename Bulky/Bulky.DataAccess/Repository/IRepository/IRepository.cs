7584b711fb69c052e9959e787b8c137b55b3e5aa
shajigholam <shajigholam@myseneca.ca>
2024-04-19 11:07:12 -0400

    Added Repository Implementation

--- Bulky/Bulky.DataAccess/Repository/IRepository/IRepository.cs
+++ Bulky/Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -1,8 +1,16 @@
 ﻿using System;
+using System.Linq.Expressions;
+
 namespace Bulky.DataAccess.Repository.IRepository
 {
-	public interface IRepository
+	public interface IRepository<T> where T : class
 	{
-	}
+		//T - can be a generic model that we want to do CRUD operation on it
+		IEnumerable<T> GetAll();
+		T Get(Expression<Func<T, bool>> filter);
+		void Add(T entity);
+        void Remove(T entity);
+		void RemoveRange(IEnumerable<T> entity);
+    }
 }
 
