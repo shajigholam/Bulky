da1b387864df6a7c7e02169e89b853d974197bba
shajigholam <shajigholam@myseneca.ca>
2024-04-19 12:59:06 -0400

    Implemented Category Repository

---
+++ Bulky/Bulky.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Bulky.Models;
+
+namespace Bulky.DataAccess.Repository.IRepository
+{
+	public interface ICategoryRepository : IRepository<Category>
+	{
+		void Update(Category obj);
+		void Save();
+	}
+}
+
