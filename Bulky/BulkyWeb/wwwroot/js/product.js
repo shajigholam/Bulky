99b3831e7160f8c1ea25510113da3ea2dc79de6b
shajigholam <samanehhajigholam@yahoo.co.nz>
2024-05-08 21:14:23 -0400

    fixed the bug

--- Bulky/BulkyWeb/wwwroot/js/product.js
+++ Bulky/BulkyWeb/wwwroot/js/product.js
@@ -6,9 +6,8 @@ $(document).ready(function () {
 
 function loadDataTable() {
     dataTable = $('#tblData').DataTable({
-        "ajax": { url:'/admin/product/getall'},
+        "ajax": { url: '/admin/product/getall' },
         "columns": [
-            // name of the data should be exactly the same as the ones from API
             { data: 'title', "width": "25%" },
             { data: 'isbn', "width": "15%" },
             { data: 'listPrice', "width": "10%" },
@@ -18,8 +17,8 @@ function loadDataTable() {
                 data: 'id',
                 "render": function (data) {
                     return `<div class="w-75 btn-group" role="group">
-                    <a href="/admin/product/upsert?${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>
-                    <a onClick=Delete('/admin/product/delete/${data}' class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+                     <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>               
+                     <a onClick=Delete('/admin/product/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
                     </div>`
                 },
                 "width": "25%"
@@ -27,15 +26,16 @@ function loadDataTable() {
         ]
     });
 }
+
 function Delete(url) {
     Swal.fire({
-        title: "Are you sure?",
+        title: 'Are you sure?',
         text: "You won't be able to revert this!",
-        icon: "warning",
+        icon: 'warning',
         showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, delete it!"
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
     }).then((result) => {
         if (result.isConfirmed) {
             $.ajax({
@@ -47,5 +47,5 @@ function Delete(url) {
                 }
             })
         }
-    });
+    })
 }
