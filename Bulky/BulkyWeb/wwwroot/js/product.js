8256a8a7469c95dd12e4773f7148f445c1a520ee
shajigholam <samanehhajigholam@yahoo.co.nz>
2024-05-08 21:11:53 -0400

    not working

--- Bulky/BulkyWeb/wwwroot/js/product.js
+++ Bulky/BulkyWeb/wwwroot/js/product.js
@@ -1,3 +1,5 @@
+var dataTable;
+
 $(document).ready(function () {
     loadDataTable();
 });
@@ -17,7 +19,7 @@ function loadDataTable() {
                 "render": function (data) {
                     return `<div class="w-75 btn-group" role="group">
                     <a href="/admin/product/upsert?${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>
-                    <a href="/admin/product/delete/${data}" class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+                    <a onClick=Delete('/admin/product/delete/${data}' class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
                     </div>`
                 },
                 "width": "25%"
@@ -40,6 +42,7 @@ function Delete(url) {
                 url: url,
                 type: 'DELETE',
                 success: function (data) {
+                    dataTable.ajax.reload();
                     toastr.success(data.message);
                 }
             })
