cdda4d057f4fdc698e1ebd07a13de65a4287dc07
shajigholam <shajigholam@myseneca.ca>
2024-05-09 09:03:50 -0400

    Identity Tables

--- Bulky/Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky/Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -116,7 +116,7 @@ namespace Bulky.DataAccess.Data
                     ImageUrl = ""
                 }
                 );
-            //after installing identity package, add related tables by add-migration addIdentityTables then update-database
+            //after installing identity package, to add identity tables by add-migration addIdentityTables then update-database
         }
     }
 }
