1e6b4df5ab5b09eecf5e1ca9599ae3e97e495f3f
shajigholam <shajigholam@myseneca.ca>
2024-05-10 11:08:46 -0400

    Revert "extend identity user"
    This reverts commit 0b462e1c7d270a89871455e5302cafb305f73774.

--- Bulky/Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky/Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -14,10 +14,10 @@ namespace Bulky.DataAccess.Data
 		{
 
 		}
+
         //create table (dotnet ef migrations add AddCategoryTableToDb)
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
-        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
