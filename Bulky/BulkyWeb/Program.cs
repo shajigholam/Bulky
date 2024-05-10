474acddb442f35a2034479cd19e4e96fc627681a
shajigholam <shajigholam@myseneca.ca>
2024-05-10 11:07:23 -0400

    Revert "create roles in db"
    This reverts commit 22939010f956329a5ddbfe0dc284e19fb3984be1.

--- Bulky/BulkyWeb/Program.cs
+++ Bulky/BulkyWeb/Program.cs
@@ -3,8 +3,6 @@ using Bulky.DataAccess.Repository;
 using Bulky.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
-using Bulky.Utility;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,11 +12,11 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
 // Add UnitOfWork in the dependency inj - using add scoped lifetime(for 1 request it will use the same service)
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-builder.Services.AddScoped<IEmailSender, EmailSender>();
+
 
 var app = builder.Build();
 
