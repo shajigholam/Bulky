47ea94759352fe0c50fe60f960b31697adbcdf5e
shajigholam <samanehhajigholam@yahoo.co.nz>
2024-05-20 19:45:23 -0400

    summary get action method

--- Bulky/Bulky.Models/ViewModel/ShoppingCartVM.cs
+++ Bulky/Bulky.Models/ViewModel/ShoppingCartVM.cs
@@ -9,6 +9,7 @@ namespace Bulky.Models.ViewModel
     public class ShoppingCartVM
     {
         public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
-        public double OrderTotal { get; set; }
+        public OrderHeader OrderHeader { get; set; }
+        //public double OrderTotal { get; set; }
     }
 }
